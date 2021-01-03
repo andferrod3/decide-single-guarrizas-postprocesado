fe12be5eefbf88ca7aad76ae01706f794219ad94
Javier <72518346+javrodsan5@users.noreply.github.com>
2021-01-03 14:32:04 +0100

    Imperiali y test de que funciona

--- decide/postproc/views.py
+++ decide/postproc/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+import math
 
 
 class PostProcView(APIView):
@@ -16,6 +17,46 @@ class PostProcView(APIView):
         out.sort(key=lambda x: -x['postproc'])
         return Response(out)
 
+    def imperialiYResiduo(self, numEscanos, options):
+
+        votosTotales = 0
+        for x in options:
+            votosTotales += x['votes']
+
+        if votosTotales > 0 and numEscanos > 0:
+
+            q = round(votosTotales / (numEscanos+2), 0)
+            
+            escanosAsignados = 0
+            for x in options:
+                escanosSuelo = math.trunc(x['votes']/q)
+                x.update({'escanosImp' : escanosSuelo})
+                escanosAsignados += x['escanosImp']               
+
+            #Si quedan escaños libres
+            while(escanosAsignados < numEscanos):
+                for x in options:
+                    x.update({ 
+                        'escanosRes' : x['votes'] - (q * x['escanosImp'])})
+
+                options.sort(key=lambda x : -x['escanosRes'])
+
+                opcionMasVotosResiduo = options[0]
+                opcionMasVotosResiduo.update({
+                'escanosImp' : opcionMasVotosResiduo['escanosImp'] + 1})
+                escanosAsignados += 1
+
+                #Lo borramos para que no este como atributo
+                for i in options:
+                    i.pop('escanosRes')
+            options.sort(key=lambda x : -x['escanosImp'])
+            
+            return Response(options)
+        else:
+            for x in options:
+                x.update({'escanosImp' : 0})
+            return Response(options)
+
     def post(self, request):
         """
          * type: IDENTITY | EQUALITY | WEIGHT
@@ -31,8 +72,11 @@ class PostProcView(APIView):
 
         t = request.data.get('type', 'IDENTITY')
         opts = request.data.get('options', [])
-
+        numEscanos = request.data.get('numEscanos', 0)
+        
         if t == 'IDENTITY':
             return self.identity(opts)
+        elif t == 'IMPERIALI':
+            return self.imperialiYResiduo(numEscanos, opts)
 
         return Response({})
