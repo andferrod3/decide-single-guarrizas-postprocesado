d6652956cdf295263e1258a33d7dbeb876ec8a08
Javier <72518346+javrodsan5@users.noreply.github.com>
2021-01-03 14:32:31 +0100

    Imperiali y test de que funciona

--- decide/postproc/tests.py
+++ decide/postproc/tests.py
@@ -42,3 +42,35 @@ class PostProcTestCase(APITestCase):
 
         values = response.json()
         self.assertEqual(values, expected_result)
+    
+    def testImperialiFunciona(self):
+        #Test de ejemplo
+        data = {
+            'type': 'IMPERIALI',
+            'options': [
+                { 'option': 'A', 'number': 1, 'votes': 391000 },
+                { 'option': 'B', 'number': 2, 'votes': 311000 },
+                { 'option': 'C', 'number': 3, 'votes': 184000 },
+                { 'option': 'D', 'number': 4, 'votes': 73000 },
+                { 'option': 'E', 'number': 5, 'votes': 27000 },
+                { 'option': 'F', 'number': 6, 'votes': 12000 },
+                { 'option': 'G', 'number': 7, 'votes': 2000 },
+            ],
+            'numEscanos': 21,
+        }
+
+        expected_result = [
+            { 'option': 'A', 'number': 1, 'votes': 391000, 'escanosImp': 9},
+            { 'option': 'B', 'number': 2, 'votes': 311000, 'escanosImp': 7},
+            { 'option': 'C', 'number': 3, 'votes': 184000, 'escanosImp': 4},
+            { 'option': 'D', 'number': 4, 'votes': 73000, 'escanosImp': 1},
+            { 'option': 'E', 'number': 5, 'votes': 27000, 'escanosImp': 0},
+            { 'option': 'F', 'number': 6, 'votes': 12000, 'escanosImp': 0},
+            { 'option': 'G', 'number': 7, 'votes': 2000, 'escanosImp': 0},
+        ]
+
+        response = self.client.post('/postproc/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+
+        values = response.json()
+        self.assertEqual(values, expected_result)
