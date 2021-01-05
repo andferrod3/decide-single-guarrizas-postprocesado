152be663aba0bc241a607d47fff2bf360448e677
EGC-pabcubcru <pabcubcru@alum.us.es>
2021-01-05 20:28:54 +0000

    Finalizados test de método de recuento d'Hont

--- decide/postproc/tests.py
+++ decide/postproc/tests.py
@@ -269,7 +269,7 @@ class PostProcTestCase(APITestCase):
         values = response.json()
         self.assertEqual(values, expected_result)
 
-    def testDHont(self):
+    def testDHont1(self): #Número de votos para facilitar la comprobación manual.
         data = {
             'type': 'DHONT',
             'options': [
@@ -290,6 +290,145 @@ class PostProcTestCase(APITestCase):
             {'option':'OPT4','number':4,'votes': 205000, 'escanos': 4},
             {'option':'OPT5','number':5,'votes': 150000, 'escanos': 2},
             {'option':'OPT6','number':6,'votes': 16000, 'escanos': 0}
+        ]
+
+        response = self.client.post('/postproc/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+
+        values = response.json()
+        self.assertEqual(values, expected_result)
+
+    def testDHont2(self): #Número de votos muy repartido.
+        data = {
+            'type': 'DHONT',
+            'options': [
+                {'option':'OPT1','number':1,'votes': 646556},
+                {'option':'OPT2','number':2,'votes': 452154},
+                {'option':'OPT3','number':3,'votes': 481216},
+                {'option':'OPT4','number':4,'votes': 848654},
+                {'option':'OPT5','number':5,'votes': 879564},
+                {'option':'OPT6','number':6,'votes': 648321},
+                {'option':'OPT7','number':7,'votes': 143210},
+                {'option':'OPT8','number':8,'votes': 896483},
+                {'option':'OPT9','number':9,'votes': 874684},
+                {'option':'OPT10','number':10,'votes': 648545}
+            ],
+            'numEscanos': 32
+        }
+
+        expected_result = [
+            {'option':'OPT1','number':1,'votes': 646556,'escanos': 3},
+            {'option':'OPT2','number':2,'votes': 452154,'escanos': 2},
+            {'option':'OPT3','number':3,'votes': 481216,'escanos': 2},
+            {'option':'OPT4','number':4,'votes': 848654,'escanos': 4},
+            {'option':'OPT5','number':5,'votes': 879564,'escanos': 5},
+            {'option':'OPT6','number':6,'votes': 648321,'escanos': 3},
+            {'option':'OPT7','number':7,'votes': 143210,'escanos': 0},
+            {'option':'OPT8','number':8,'votes': 896483,'escanos': 5},
+            {'option':'OPT9','number':9,'votes': 874684,'escanos': 5},
+            {'option':'OPT10','number':10,'votes': 648545,'escanos': 3}
+        ]
+
+        response = self.client.post('/postproc/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+
+        values = response.json()
+        self.assertEqual(values, expected_result)
+
+    def testDHont3(self): #No se le indica el número de escaños a repartir.
+        data = {
+            'type': 'DHONT',
+            'options': [
+                {'option':'OPT1','number':1,'votes': 32142},
+                {'option':'OPT2','number':2,'votes': 64315},
+                {'option':'OPT3','number':3,'votes': 97845},
+                {'option':'OPT4','number':4,'votes': 31645},
+                {'option':'OPT5','number':5,'votes': 97645}
+            ]#,
+            #'numEscanos': 50
+        }
+
+        expected_result = [
+            {'option':'OPT1','number':1,'votes': 32142,'escanos': 0},
+            {'option':'OPT2','number':2,'votes': 64315,'escanos': 0},
+            {'option':'OPT3','number':3,'votes': 97845,'escanos': 0},
+            {'option':'OPT4','number':4,'votes': 31645,'escanos': 0},
+            {'option':'OPT5','number':5,'votes': 97645,'escanos': 0}
+        ]
+
+        response = self.client.post('/postproc/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+
+        values = response.json()
+        self.assertEqual(values, expected_result)
+
+    def testDHont4(self): #Número de votos iguales.
+        data = {
+            'type': 'DHONT',
+            'options': [
+                {'option':'OPT1','number':1,'votes': 34124611},
+                {'option':'OPT2','number':2,'votes': 34124611}
+            ],
+            'numEscanos': 20
+        }
+
+        expected_result = [
+            {'option':'OPT1','number':1,'votes': 34124611,'escanos': 10},
+            {'option':'OPT2','number':2,'votes': 34124611,'escanos': 10}
+        ]
+
+        response = self.client.post('/postproc/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+
+        values = response.json()
+        self.assertEqual(values, expected_result)
+
+    def testDHont5(self): #Número de votos muy desigual.
+        data = {
+            'type': 'DHONT',
+            'options': [
+                {'option':'OPT1','number':1,'votes': 65155},
+                {'option':'OPT2','number':2,'votes': 89498},
+                {'option':'OPT3','number':3,'votes': 8645151},
+                {'option':'OPT4','number':4,'votes': 65311},
+                {'option':'OPT5','number':5,'votes': 8784565},
+                {'option':'OPT6','number':6,'votes': 32151},
+                {'option':'OPT7','number':7,'votes': 987515}
+            ],
+            'numEscanos': 100
+        }
+
+        expected_result = [
+            {'option':'OPT1','number':1,'votes': 65155,'escanos': 0},
+            {'option':'OPT2','number':2,'votes': 89498,'escanos': 0},
+            {'option':'OPT3','number':3,'votes': 8645151,'escanos': 47},
+            {'option':'OPT4','number':4,'votes': 65311,'escanos': 0},
+            {'option':'OPT5','number':5,'votes': 8784565,'escanos': 48},
+            {'option':'OPT6','number':6,'votes': 32151,'escanos': 0},
+            {'option':'OPT7','number':7,'votes': 987515,'escanos': 5}
+        ]
+
+        response = self.client.post('/postproc/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+
+        values = response.json()
+        self.assertEqual(values, expected_result)
+
+    def testDHont6(self): #Número de votos muy grandes.
+        data = {
+            'type': 'DHONT',
+            'options': [
+                {'option':'OPT1','number':1,'votes': 846245435646644},
+                {'option':'OPT2','number':2,'votes': 931654454532151},
+                {'option':'OPT3','number':3,'votes': 821564325158125}
+            ],
+            'numEscanos': 200
+        }
+
+        expected_result = [
+            {'option':'OPT1','number':1,'votes': 846245435646644,'escanos': 65},
+            {'option':'OPT2','number':2,'votes': 931654454532151,'escanos': 72},
+            {'option':'OPT3','number':3,'votes': 821564325158125,'escanos': 63}
         ]
 
         response = self.client.post('/postproc/', data, format='json')
