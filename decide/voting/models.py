22c7d64b89dda108c58ca9e1fdf473d0e282aa28
EGC-pabcubcru <pabcubcru@alum.us.es>
2021-01-13 17:41:03 +0000

    Añadida opción PREGUNTASPESO en el tipo de votación en módulo Voting

--- decide/voting/models.py
+++ decide/voting/models.py
@@ -39,7 +39,7 @@ class Voting(models.Model):
     end_date = models.DateTimeField(blank=True, null=True)
 
     tipo_votacion = [("IDENTITY", "IDENTITY"), ("IMPERIALI", "IMPERIALI"), ("HUNTINGTONHILL", "HUNTINGTONHILL"), ("DANISH", "DANISH"),
-    ("DHONT", "DHONT"), ("MULTIPREGUNTAS", "MULTIPREGUNTAS"), ("SAINTELAGUE", "SAINTELAGUE")]
+    ("DHONT", "DHONT"), ("MULTIPREGUNTAS", "MULTIPREGUNTAS"), ("SAINTELAGUE", "SAINTELAGUE"), ("PREGUNTASPESO", "PREGUNTASPESO")]
 
     tipo = models.CharField(choices=tipo_votacion, max_length=20, default="IDENTITY", verbose_name='Count method')
 
